7ce599cc27b6a25a8263830de7ef3e6a4f5524a6
Jicazo <juliopicazo06@gmail.com>
2024-06-09 15:50:28 -0600

    chore: Update HTML and CSS classes for home page and card component

--- src/app/gifs/components/card/card.component.ts
+++ src/app/gifs/components/card/card.component.ts
@@ -9,6 +9,7 @@ import { Gif } from '../../interfaces/gifs.interface';
 @Component({
   selector: 'gifs-card',
   templateUrl: 'card.component.html',
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit{
 
